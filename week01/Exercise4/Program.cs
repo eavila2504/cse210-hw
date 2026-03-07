ba2eda1fbddd41858e5c89049b11d84921b92aff
eavila2504 <eavilaflores@byupathway.edu>
2026-03-07 13:46:22 -0600

    numbers list

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int inputNumber = -1;
+        while (inputNumber != 0)
+        {
+            Console.Write("Please, enter a number (press 0 to quit): ");
+            string answer = Console.ReadLine();
+            inputNumber = int.Parse(answer);
+
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int maxNumber = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+
+            }
+        }
+        Console.WriteLine($"The greatest number is {maxNumber} ");
     }
+
 }
