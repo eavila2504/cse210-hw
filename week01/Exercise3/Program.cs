8627efbf3e7703362196395fd19739ab2a4a311e
eavila2504 <eavilaflores@byupathway.edu>
2026-03-06 23:24:10 -0600

    guess it!

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string restart = "yes";
+
+        while (restart == "yes")
+        {
+            Random random = new Random();
+            int unknownNumber = random.Next(1, 101);
+            int guess = -1;
+
+            while (guess != unknownNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < unknownNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > unknownNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.Write("Play again? (yes/no): ");
+            restart = Console.ReadLine();
+        }
     }
 }
