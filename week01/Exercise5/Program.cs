296e3fa419a00fb777db2327f0a125bea9e25e60
eavila2504 <eavilaflores@byupathway.edu>
2026-03-07 14:49:07 -0600

    square

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber();
+        int SquaredNumber = SquareNumber(UserNumber);
+        DisplayResult(UserName, SquaredNumber);
+
+    }
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!"); 
+        }
+
+        static string PromptUserName()
+    {
+        Console.Write("Please Enter your full name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+        static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int FavoriteNumber = int.Parse(Console.ReadLine());
+        
+        return FavoriteNumber;
+    }
+
+        static int SquareNumber(int FavoriteNumber)
+    {
+        int Square = FavoriteNumber*FavoriteNumber;
+
+        return Square;
+    }
+    
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name} the square of your number is {square}");
     }
 }
