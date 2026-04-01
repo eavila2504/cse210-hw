3441dd4b3a65e55216baecc0d326ec1b4ec43e47
eavila2504 <eavilaflores@byupathway.edu>
2026-03-31 21:33:03 -0600

    middle of the code

---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+    
+       
+}
