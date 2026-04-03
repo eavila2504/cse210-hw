8f0b95cc6f3380263752afebb8a06d9f65c4d145
eavila2504 <eavilaflores@byupathway.edu>
2026-04-02 23:46:09 -0600

    changes

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,27 @@
+public class BreathingActivity : Activity
+{
+   public BreathingActivity() : base("Breathing Activity", "This activity will help you relax and focus on your breathing." , 0)
+    {     
+    }
+
+    public void Run()
+    {
+        StartingMessage();
+
+        DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine("\n Breathe in...");
+            ShowCountdown(4);
+
+            if (DateTime.Now >= endTime) break;
+
+            Console.WriteLine("\n Breathe out...");
+            ShowCountdown(6);
+        }
+
+        EndingMessage();
+        
+    }
+}
---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,20 @@
+public class ListingActivity : Activity
+{
+  private int _count;
+  private List<string> _prompts = new List<string>
+  {
+    "Who are people that you appreciate?",
+    "What are personal strengths of yours?",
+    "Who are people that you have helped this week?",
+    "When have you felt the Holy Ghost this month?",
+    "Who are some of your personal heroes?"
+  };
+
+  public ListingActivity()
+    {
+        _name = "Listing Activity";
+        _descrption = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+        _duration = 50;
+    }
+
+}
